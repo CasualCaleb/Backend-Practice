84cca1f829e548520ef0a8b2330c4854281d26ea
casualcaleb <cookiecaleb@gmail.com>
2026-09-10 14:13:47 -0700

    Added a few comments

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ from routes import users, auth, admin
 from starlette.middleware.sessions import SessionMiddleware
 from db import init_db
 
+# Load .env
 load_dotenv()
 
+# Initiate FastAPI and SQLite database
 app = FastAPI()
 init_db()
 
+# Add middleware
 app.add_middleware(
     SessionMiddleware,
     secret_key=os.getenv("SESSION_SECRET"),
 )
 
+# Setup routes
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(admin.router)
